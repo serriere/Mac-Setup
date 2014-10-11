255a154e1d618c059150899d1e751431895f39a5
Alex Serriere <alex@serriere.com>
2014-10-11 13:54:23 -0700

    Add more items

--- setup-script.sh
+++ setup-script.sh
@@ -13,3 +13,28 @@ fi
 
 # Update homebrew recipes
 brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+# Update the $PATH in your ~/.bash_profile in order to use these tools
+# over their Mac counterparts:
+$PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+
+# Automate installation of apps with Cask
+brew install caskroom/cask/brew-cask
+
+apps=(
+  dropbox
+  google-chrome
+)
